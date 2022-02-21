72ff6617be2a38181e7836cce251a6859c5009b0
Nancy ElSharkawi <elsharkawinancy@gmail.com>
2022-02-21 22:02:08 +0300

    done

--- Random/script.js
+++ Random/script.js
@@ -1,26 +1,3 @@
 // project أنشئ كائن
 
-const project = {
-  type: "tech",
-  users: 1000000,
-  biography() {
-    return "Our company serves the blind who like to use tech products.";
-  },
-};
-
 // randomProperty أنشئ دالة
-
-function randomProperty(x) {
-  // Object of Keys
-  // {0: "type", 1: "users", 2: "biography"}
-  const keysObject = Object.keys(x);
-  // Random Number between 0 and Object.length -1
-  const randomNumber = Math.floor(Math.random() * Object.keys(x).length);
-  // Print Random Object Property
-  //   Key
-  console.log(keysObject[`${randomNumber}`]);
-  //   Value
-  console.log(x[`${keysObject[`${randomNumber}`]}`]);
-}
-
-randomProperty(project);
